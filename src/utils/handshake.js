f462693a39b447fa5a4983d97d7411cf77fe823f
youshen2 <178326018+youshen2@users.noreply.github.com>
2026-03-14 17:38:31 +0800

    版本号

--- src/utils/handshake.js
+++ src/utils/handshake.js
@@ -2,8 +2,8 @@ import interconn from './interconn.js';
 import { versionCode } from "../manifest.json";
 import router from '@system.router';
 
-const MIN_PHONE_VERSION = 126201;
-const MIN_PHONE_VERSION_NAME = "V26.2.1";
+const MIN_PHONE_VERSION = 126202;
+const MIN_PHONE_VERSION_NAME = "V26.2.2";
 const type = "__hs__"
 const TIMEOUT = 15000;
 
